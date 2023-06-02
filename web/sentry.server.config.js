2e105791963b02e225fff028087e598644cd9a6c
mushroom <wuminmiao@gmail.com>
2023-06-02 15:22:14 +0800

    fix:update sentry web config

--- web/sentry.server.config.js
+++ web/sentry.server.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: 'https://6bf48a450f054d749398c02a61bae343@o4505264807215104.ingest.sentry.io/4505264809115648',
+  dsn: 'https://ee554481ba434e8b94bf76657ef9e804@o4505265719410688.ingest.sentry.io/4505265722032128',
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
